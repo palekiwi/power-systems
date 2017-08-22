1e9f3df01820ee14969bd225b1d79c278c3d7a7e
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-08-22 15:41:55 +0800

    reset activeStructure on close SystemViewerModal

--- src/reducers/activeStructureReducer.js
+++ src/reducers/activeStructureReducer.js
@@ -3,8 +3,13 @@ import * as types from '../constants/actionTypes.js';
 
 export default function activeStructure (state = initialState.activeStructure, action) {
   switch (action.type) {
+
   case types.SET_ACTIVE_STRUCTURE:
     return action.payload;
+
+  case types.CLOSE_SYSTEM_VIEWER_MODAL:
+    return null;
+
   default:
     return state;
   }
