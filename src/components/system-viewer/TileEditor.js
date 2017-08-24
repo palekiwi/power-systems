65fcafce3e278ca9af925abfe01b059640f30255
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-08-24 14:42:19 +0800

    implement saveTerrainTile

--- src/components/system-viewer/TileEditor.js
+++ src/components/system-viewer/TileEditor.js
@@ -2,24 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import tile from '../../lib/tile.js';
 import * as tt from '../../data/terrain-textures.js';
+import values from 'ramda/src/values';
 import './TileEditor.scss';
 
 class TileEditor extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      type: 'structureTile',
+      type: 'structureTiles',
       texture: {},
       data: {}
     };
+
+    this.saveTerrainTile = this.saveTerrainTile.bind(this);
+    this.saveStructureTile = this.saveStructureTile.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.setActiveType = this.setActiveType.bind(this);
   }
-  saveTile () {
-    const {type, texture, data} = this.state;
+
+  saveStructureTile () {
+    const {texture, data} = this.state;
     const position = this.props.activeTile;
     this.props.saveTile({
-      type,
+      type: 'structureTiles',
       tile: tile({texture, position, data})
     });
+  }
+
+  saveTerrainTile (texture) {
+    const position = this.props.activeTile;
+    this.props.saveTile({
+      type: 'terrainTiles',
+      tile: tile({texture, position})
+    });
+  }
+
+  closeModal () {
+    this.props.resetActiveTile();
+  }
+
+  setActiveType (type) {
+    this.setState({type});
   }
 
   render () {
@@ -30,7 +53,36 @@ class TileEditor extends React.Component {
         >
         </div>
         <div className="TileEditor__Content">
-          <button onClick={() => this.saveTile()}>Save</button>
+          <div>
+            <button onClick={() => this.setActiveType('terrainTiles')}>Terrain</button>
+            <button onClick={() => this.setActiveType('structureTiles')}>Structures</button>
+          </div>
+
+          { this.state.type == 'terrainTiles' &&
+            <div>
+              <h3>Terrain</h3>
+              <div>
+                {values(tt).map((t,i) =>
+                  <span key={i}
+                    onClick={() => this.saveTerrainTile(t)}
+                  >
+                    <img src={require('../../assets/' + t.filename)}/>
+                  </span>
+                )}
+              </div>
+            </div>
+          }
+
+          { this.state.type == 'structureTiles' &&
+            <div>
+              <h3>Structures</h3>
+            </div>
+          }
+
+          <div>
+            <button onClick={this.closeModal}>Cancel</button>
+          </div>
+
         </div>
       </div>
     );
