93e3de95010ba5df0b2bf3dd832517f8b2a1c860
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-08-30 18:34:30 +0800

    compute and set variable consumption and output

--- src/lib/power-system.js
+++ src/lib/power-system.js
@@ -1,14 +1,26 @@
+/* eslint-disable no-console */
 import { hourly } from './helpers/dateArrays.js';
 import R from 'ramda';
 
+const computeVariable = x => R.map(R.multiply(R.prop('capacity', x)), R.prop('variation', x));
+const zipHourly = R.zipWith((date, val) => ({date, val}), hourly);
+const setVariable = field => x => R.set(R.lensProp(field), R.compose(zipHourly, computeVariable)(x), x);
+const setCons = setVariable('consumption');
+const setOutput = setVariable('consumption');
+
 export function computeSystemOutput (sts) {
-  const load = sts.filter(R.propEq('class', 'consumer'));
-  /* eslint-disable no-console */
-  console.log(R.map(setCons, load)[0]);
+  const consumers = sts
+    .filter(R.propEq('class', 'consumer'))
+    .map(setCons);
+
+  const primary = sts
+    .filter(R.propEq('priority', 2))
+    .map(setOutput);
+
+  const secondary = sts.filter(R.propEq('priority', 1));
+  const backup = sts.filter(R.propEq('priority', 0));
+
+  console.log(primary[0]);
+
   return sts;
 }
-
-const computeCons = x => R.map(R.multiply(R.prop('capacity', x)), R.prop('variation', x));
-const zipHourly = R.zipWith((date, val) => ({date, val}), hourly);
-const consLens = R.lensProp('consumption');
-const setCons = x => R.set(consLens, R.compose(zipHourly, computeCons)(x), x);
