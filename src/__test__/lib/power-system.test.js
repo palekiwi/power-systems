3d0ee417ea9230afcdc51aa3677aaae9ff23bef3
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-08-30 18:15:42 +0800

    write function to compute and set consumption

--- src/__test__/lib/power-system.test.js
+++ src/__test__/lib/power-system.test.js
@@ -6,28 +6,28 @@ describe('computeSystemOutput', () => {
   describe('given array of structureTiles', () => {
     it('computes consumption and output of each component', () => {
       const tiles = [
+        {name: 'hospital', class: 'consumer', type: 'variable', capacity: 100, variation: [0.4, 0.4, 0.9, 0.3], consumption: null},
+        {name: 'communityCenter', class: 'consumer', type: 'variable', capacity: 100, variation: [0.3, 0.5, 0.7, 0.4], consumption: null},
         {name: 'solar', class: 'generator', type: 'variable', capacity: 100, priority: 2, variation: [0, 0.1, 0.5, 0], output: null},
         {name: 'gas', class: 'generator', type: 'non-variable', capacity: 100, priority: 1, output: null},
-        {name: 'diesel', class: 'generator', type: 'non-variable', capacity: 100, priority: 0, output: null},
-        {name: 'hospital', class: 'consumer', type: 'variable', capacity: 100, variation: [0.4, 0.4, 0.9, 0.3], consumption: null},
-        {name: 'communityCenter', class: 'consumer', type: 'variable', capacity: 100, variation: [0.3, 0.5, 0.7, 0.4], consumption: null}
+        {name: 'diesel', class: 'generator', type: 'non-variable', capacity: 100, priority: 0, output: null}
       ];
 
       const expected = [
-        zipToDates([0, 10, 50, 0]),
-        zipToDates([70, 80, 90, 70]),
-        zipToDates([0, 0, 70, 0]),
         zipToDates([40, 40, 90, 30]),
-        zipToDates([30, 50, 70, 40])
+        zipToDates([30, 50, 70, 40]),
+        zipToDates([0,  10, 50,  0]),
+        zipToDates([70, 80, 90, 70]),
+        zipToDates([0,   0, 20,  0])
       ];
 
       const res = computeSystemOutput(tiles);
 
-      expect(res[0].output).toEqual(expected[0]);
-      expect(res[1].output).toEqual(expected[1]);
+      expect(res[0].consumption).toEqual(expected[0]);
+      expect(res[1].consumption).toEqual(expected[1]);
       expect(res[2].output).toEqual(expected[2]);
-      expect(res[3].consumption).toEqual(expected[3]);
-      expect(res[4].consumption).toEqual(expected[4]);
+      expect(res[3].output).toEqual(expected[3]);
+      expect(res[4].output).toEqual(expected[4]);
     });
   });
 });
--- src/lib/power-system.js
+++ src/lib/power-system.js
@@ -1,3 +1,14 @@
+import { hourly } from './helpers/dateArrays.js';
+import R from 'ramda';
+
 export function computeSystemOutput (sts) {
+  const load = sts.filter(R.propEq('class', 'consumer'));
+  /* eslint-disable no-console */
+  console.log(R.map(setCons, load)[0]);
   return sts;
 }
+
+const computeCons = x => R.map(R.multiply(R.prop('capacity', x)), R.prop('variation', x));
+const zipHourly = R.zipWith((date, val) => ({date, val}), hourly);
+const consLens = R.lensProp('consumption');
+const setCons = x => R.set(consLens, R.compose(zipHourly, computeCons)(x), x);
