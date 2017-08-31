04e4e9bb9e213080fd3d543dcf2f2263861278a7
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-08-31 18:09:02 +0800

    write test for sufficient primary power

--- src/__test__/lib/power-system.test.js
+++ src/__test__/lib/power-system.test.js
@@ -24,11 +24,82 @@ describe('computeSystemOutput', () => {
       const res = computeSystemOutput(tiles);
 
       expect(res.length).toEqual(expected.length);
-      expect(res[0].power).toEqual(expected[0]);
-      expect(res[1].power).toEqual(expected[1]);
-      expect(res[2].power).toEqual(expected[2]);
-      expect(res[3].power).toEqual(expected[3]);
-      expect(res[4].power).toEqual(expected[4]);
+      expect(res.find(el => el.name == 'hospital').power).toEqual(expected[0]);
+      expect(res.find(el => el.name == 'communityCenter').power).toEqual(expected[1]);
+      expect(res.find(el => el.name == 'solar').power).toEqual(expected[2]);
+      expect(res.find(el => el.name == 'gas').power).toEqual(expected[3]);
+      expect(res.find(el => el.name == 'diesel').power).toEqual(expected[4]);
+    });
+  });
+
+  describe('given primary power supplies enough power', () => {
+    it('secondary and backup should output nothing', () => {
+      const tiles = [
+        {name: 'hospital', class: 'consumer', type: 'variable', capacity: 100, variation: zipToDates([0.1, 0.1, 0.1, 0.1]), power: null},
+        {name: 'communityCenter', class: 'consumer', type: 'variable', capacity: 100, variation: zipToDates([0.1, 0.1, 0.1, 0.1]), power: null},
+        {name: 'solar', class: 'generator', type: 'variable', capacity: 100, priority: 2, variation: zipToDates([0, 0.3, 0.5, 0]), power: null},
+        {name: 'gas', class: 'generator', type: 'non-variable', capacity: 100, priority: 1, power: null},
+        {name: 'diesel', class: 'generator', type: 'non-variable', capacity: 100, priority: 0, power: null}
+      ];
+
+      const expected = [
+        zipToDates([10, 10,  10, 10]),
+        zipToDates([10, 10,  10, 10]),
+        zipToDates([0,  30,  50,  0]),
+        zipToDates([20,  0,   0, 20]),
+        zipToDates([0,   0,   0,  0])
+      ];
+
+      const res = computeSystemOutput(tiles);
+
+      expect(res.length).toEqual(expected.length);
+      expect(res.find(el => el.name == 'hospital').power).toEqual(expected[0]);
+      expect(res.find(el => el.name == 'communityCenter').power).toEqual(expected[1]);
+      expect(res.find(el => el.name == 'solar').power).toEqual(expected[2]);
+      expect(res.find(el => el.name == 'gas').power).toEqual(expected[3]);
+      expect(res.find(el => el.name == 'diesel').power).toEqual(expected[4]);
+    });
+  });
+
+  describe('given structureTiles without primary power', () => {
+    it('computes consumption and output of each component', () => {
+      const tiles = [
+        {name: 'hospital', class: 'consumer', type: 'variable', capacity: 100, variation: zipToDates([0.4, 0.4, 0.9, 0.3]), power: null},
+        {name: 'communityCenter', class: 'consumer', type: 'variable', capacity: 100, variation: zipToDates([0.3, 0.5, 0.7, 0.4]), power: null},
+        {name: 'gas', class: 'generator', type: 'non-variable', capacity: 100, priority: 1, power: null},
+        {name: 'diesel', class: 'generator', type: 'non-variable', capacity: 100, priority: 0, power: null}
+      ];
+
+      const expected = [
+        zipToDates([40, 40,  90, 30]),
+        zipToDates([30, 50,  70, 40]),
+        zipToDates([70, 90, 100, 70]),
+        zipToDates([0,   0,  60,  0])
+      ];
+
+      const res = computeSystemOutput(tiles);
+
+      expect(res.length).toEqual(expected.length);
+      expect(res.find(el => el.name == 'hospital').power).toEqual(expected[0]);
+      expect(res.find(el => el.name == 'communityCenter').power).toEqual(expected[1]);
+      expect(res.find(el => el.name == 'gas').power).toEqual(expected[2]);
+      expect(res.find(el => el.name == 'diesel').power).toEqual(expected[3]);
+    });
+  });
+
+  describe('given array of structureTiles without load', () => {
+    it('secondary power and backup output no power', () => {
+      const tiles = [
+        {name: 'solar', class: 'generator', type: 'variable', capacity: 100, priority: 2, variation: zipToDates([0, 0.1, 0.5, 0]), power: null},
+        {name: 'gas', class: 'generator', type: 'non-variable', capacity: 100, priority: 1, power: null},
+        {name: 'diesel', class: 'generator', type: 'non-variable', capacity: 100, priority: 0, power: null}
+      ];
+
+      const res = computeSystemOutput(tiles);
+
+      expect(res.find(el => el.name == 'solar').power).toEqual(null);
+      expect(res.find(el => el.name == 'gas').power).toEqual(null);
+      expect(res.find(el => el.name == 'diesel').power).toEqual(null);
     });
   });
 });
--- src/lib/power-system.js
+++ src/lib/power-system.js
@@ -1,3 +1,4 @@
+
 /* eslint-disable no-console */
 import R from 'ramda';
 
@@ -5,11 +6,21 @@ import R from 'ramda';
 const computeVariablePower = x => R.map(R.evolve({value: R.multiply(R.prop('capacity', x))}), R.prop('variation', x));
 const setVariablePower = x => R.set(R.lensProp('power'), computeVariablePower(x), x);
 
-const computeNonVarPower = load => cap => R.map(r => (r.value <= cap) ? r : R.assoc('value', cap, r), load);
+const computeNonVarPower = load => cap => R.map(r => {
+  if (r.value <= 0) return R.assoc('value', 0, r);
+  return (r.value <= cap) ? r : R.assoc('value', cap, r);
+}, load);
+
 const setNonVarPower = load => x => R.set(R.lensProp('power'), computeNonVarPower(load)(R.prop('capacity', x)), x);
 
 const power = R.pluck('power');
-const zipValuesWith = fn => R.zipWith(R.mergeWithKey((k,l,r) => k == 'value' ? fn(l, Math.abs(r)) : r));
+const zipValuesWith = fn => (a,b) => {
+  if (R.isEmpty(a) && R.isEmpty(b)) return [];
+  if (R.isEmpty(a)) return b;
+  if (R.isEmpty(b)) return a;
+  return R.zipWith(R.mergeWithKey((k,l,r) => k == 'value' ? fn(l, Math.abs(r)) : r), a, b);
+};
+
 const zipSum = (acc, arr) => (arr.length == 0) ? acc : zipSum(zipValuesWith(R.add)(acc, R.head(arr)), R.tail(arr));
 const zipDiff = (acc, arr) => (arr.length == 0) ? acc : zipDiff(zipValuesWith(R.subtract)(acc, R.head(arr)), R.tail(arr));
 
@@ -18,6 +29,8 @@ const addValues = zipRec(zipSum);
 const subValues = zipRec(zipDiff);
 
 export function computeSystemOutput (sts) {
+  if (sts.filter(R.propEq('class', 'consumer')).length == 0) return sts;
+
   const consumers = sts
     .filter(R.propEq('class', 'consumer'))
     .map(setVariablePower);
@@ -38,7 +51,5 @@ export function computeSystemOutput (sts) {
   const backup = sts.filter(R.propEq('priority', 0))
     .map(setNonVarPower(ls));
 
-  console.log(power(backup));
-
-  return R.reduce(R.concat, [], [consumers, primary, secondary, backup]);
+  return [...consumers, ...primary, ...secondary, ...backup];
 }
