6500960cd95a8e87a6944253d0efa776f9b73020
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-09-22 18:56:49 +0800

    log csv

--- src/data/power/index.js
+++ src/data/power/index.js
@@ -2,6 +2,8 @@ import {parseCSV} from '../../helpers/format.js';
 const defaultLoad = require('./defaultLoad.csv');
 const solar = require('./solar.csv');
 
+/* eslint-disable no-console */
+console.log(defaultLoad);
 export default {
   defaultLoad: defaultLoad.map(parseCSV),
   solar: solar.map(parseCSV)
